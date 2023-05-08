c9bdb12383e7e9fd5ae54b51df0723d6ed3de203
Yuliia Bernatskaia <juliabernatskaya@gmail.com>
2023-05-08 22:54:16 +0400

    Model implemented

--- src/test/java/com/epam/wdframework/WebDriverTest.java
+++ src/test/java/com/epam/wdframework/WebDriverTest.java
@@ -2,11 +2,11 @@ package com.epam.wdframework;
 
 import com.epam.wdframework.driver.DriverSingleton;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 public abstract class WebDriverTest {
+
 	protected WebDriver webDriver;
 
 	@BeforeMethod
