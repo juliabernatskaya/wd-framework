851b97e19140d5f750b1237dd1fd73099308e19b
juliabernatskaya <71770365+juliabernatskaya@users.noreply.github.com>
2023-05-08 23:05:49 +0400

    Utils implemented (#3)

---
+++ src/main/java/com/epam/wdframework/util/TextExtractor.java
@@ -0,0 +1,16 @@
+package com.epam.wdframework.util;
+
+import java.util.regex.MatchResult;
+import java.util.regex.Pattern;
+
+public class TextExtractor {
+	private static final String TOTAL_COST_EMAIL_REGEX = "(?<=Total Estimated Monthly Cost\\s)(.*?)(?=\\n)";
+
+	public static String extractTotalCostFromEmail(String emailBody) {
+		return Pattern.compile(TOTAL_COST_EMAIL_REGEX).matcher(emailBody)
+			.results()
+			.map(MatchResult::group)
+			.findFirst()
+			.orElse(emailBody);
+	}
+}
