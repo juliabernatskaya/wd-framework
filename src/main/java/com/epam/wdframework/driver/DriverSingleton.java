97ddf3dfc4b434c5dfa4f45b904fee398a068cd5
Yuliia Bernatskaia <juliabernatskaya@gmail.com>
2023-05-29 19:29:36 +0400

    Refactoring after review - DriverSingleton refactored

--- src/main/java/com/epam/wdframework/driver/DriverSingleton.java
+++ src/main/java/com/epam/wdframework/driver/DriverSingleton.java
@@ -1,68 +1,44 @@
 package com.epam.wdframework.driver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import java.util.Arrays;
-import java.util.Map;
-import java.util.Optional;
-import java.util.function.Supplier;
-import java.util.stream.Collectors;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.Optional;
 
 public class DriverSingleton {
 
-	private static WebDriver webDriver;
+    private static final String FIREFOX = "firefox";
+    private static final String CHROME = "chrome";
+    private static final String SAFARI = "safari";
+    private static WebDriver webDriver;
 
-	public static WebDriver getDriver() {
-		if (webDriver == null) {
-			webDriver = WebDriverInitializer.getByBrowserTitle(System.getProperty("browser")).initWebDriver();
-		}
-		webDriver.manage().window().maximize();
-		return webDriver;
-	}
+    public static WebDriver getDriver() {
+        if (webDriver == null) {
+            String browser = Optional.ofNullable(System.getProperty("browser")).orElse("chrome");
+            switch (browser) {
+                case CHROME: {
+                    webDriver = WebDriverManager.chromedriver().create();
+                    break;
+                }
+                case FIREFOX: {
+                    webDriver = WebDriverManager.firefoxdriver().create();
+                    break;
+                }
+                case SAFARI: {
+                    webDriver = WebDriverManager.safaridriver().create();
+                    break;
+                }
+                default: {
+                    throw new IllegalArgumentException("'" + browser + "' is not supported browser");
+                }
+            }
+        }
+        webDriver.manage().window().maximize();
+        return webDriver;
+    }
 
-	public static void closeDriver() {
-		webDriver.quit();
-		webDriver = null;
-	}
-
-	private enum WebDriverInitializer {
-		CHROME_INITIALIZER("chrome", () -> WebDriverManager.chromedriver().setup(), ChromeDriver::new),
-		FIREFOX_INITIALIZER("firefox", () -> WebDriverManager.chromedriver().setup(), FirefoxDriver::new);
-
-		private static final Map<String, WebDriverInitializer> titleMap = Arrays.stream(values())
-			.collect(Collectors.toUnmodifiableMap(webDriverInitializer -> webDriverInitializer.title,
-				webDriverInitializer -> webDriverInitializer));
-
-		private final String title;
-		private final Runnable driverSetup;
-		private final Supplier<WebDriver> driverSupplier;
-
-		WebDriverInitializer(String title, Runnable driverSetup, Supplier<WebDriver> driverSupplier) {
-			this.title = title;
-			this.driverSetup = driverSetup;
-			this.driverSupplier = driverSupplier;
-		}
-
-		public static WebDriverInitializer getByBrowserTitle(String title) {
-			if (title == null) {
-				return CHROME_INITIALIZER;
-			}
-			return Optional.ofNullable(titleMap.get(title)).orElse(CHROME_INITIALIZER);
-		}
-
-		public WebDriver initWebDriver() {
-			driverSetup();
-			return getDriverInstance();
-		}
-
-		private void driverSetup() {
-			driverSetup.run();
-		}
-
-		private WebDriver getDriverInstance() {
-			return driverSupplier.get();
-		}
-	}
+    public static void closeDriver() {
+        webDriver.quit();
+        webDriver = null;
+    }
 }
